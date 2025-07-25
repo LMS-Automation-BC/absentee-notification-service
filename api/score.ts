21c8d943aad45b98349aaa4cf4e75ed73cdf7c3d
Mona Lisa <sofiasamuels@gmail.com>
2025-07-24 22:40:25 -0400

    get grade

--- api/score.ts
+++ api/score.ts
@@ -4,6 +4,6 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   //const { name = 'World' } = req.query
  
-
+    console.log(req.query);
   return res.status(200).json({params: req.query})
 }
