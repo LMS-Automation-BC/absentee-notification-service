91762e7f95f812fcf6e8e6371594a7c57c6d3a08
Sofia Samuel Selvaraj <sofia@mivance.com>
2025-08-07 09:47:54 -0400

    changes for url encoding

--- api/score.ts
+++ api/score.ts
@@ -3,7 +3,8 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   //const { name = 'World' } = req.query
- 
+  console.log(req.body);
+  console.log(req.toString());
     console.log(JSON.stringify(req.query));
   return res.status(200).json({params: req.query})
 }
