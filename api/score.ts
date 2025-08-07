70f7e496beabf6fd29713a614d8e13ee58823fa9
Sofia Samuel Selvaraj <sofia@mivance.com>
2025-08-07 14:49:50 -0400

    check logs

--- api/score.ts
+++ api/score.ts
@@ -3,8 +3,7 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   //const { name = 'World' } = req.query
+  console.log(req.body);
   
-  console.log(req.toString());
-  
-  return res.status(200).json({params: req.body})
+  return res.status(200).json({params: req.query})
 }
